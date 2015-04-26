f5af409b4771daf72afedd79046b20f45a1c8271
Simon Schmid <mail@sschmid.com>
2015-04-26 23:29:43 +0200

    Using latest Entitas

---
+++ Assets/Libraries/Entitas.Unity.VisualDebugging/Editor/VisualDebuggingPreferencesDrawer.cs
@@ -0,0 +1,20 @@
+﻿using Entitas.Unity;
+using UnityEditor;
+using UnityEngine;
+
+namespace Entitas.Unity.VisualDebugging {
+    public class VisualDebuggingPreferencesDrawer : IEntitasPreferencesDrawer {
+        public void Draw(EntitasPreferencesConfig config) {
+            var visualDebuggingConfig = new VisualDebuggingConfig(config);
+
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUILayout.LabelField("VisualDebugging", EditorStyles.boldLabel);
+
+            // Generated Folder
+            visualDebuggingConfig.defaultInstanceCreatorFolderPath = EditorGUILayout.TextField("DefaultInstanceCreator Folder", visualDebuggingConfig.defaultInstanceCreatorFolderPath);
+            visualDebuggingConfig.typeDrawerFolderPath = EditorGUILayout.TextField("TypeDrawer Folder", visualDebuggingConfig.typeDrawerFolderPath);
+
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
