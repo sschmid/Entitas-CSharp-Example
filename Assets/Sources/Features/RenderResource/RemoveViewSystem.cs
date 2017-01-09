a6ae64cab21d678580b9475301511474960ee317
Simon Schmid <mail@sschmid.com>
2017-01-10 00:20:23 +0100

    Remmoved group observing in RemoveViewSystem

--- Assets/Sources/Features/RenderResource/RemoveViewSystem.cs
+++ Assets/Sources/Features/RenderResource/RemoveViewSystem.cs
@@ -2,30 +2,22 @@ using System.Collections.Generic;
 using Entitas;
 using UnityEngine;
 
-public sealed class RemoveViewSystem : ISetPool, IMultiReactiveSystem, IEnsureComponents {
+public sealed class RemoveViewSystem : IMultiReactiveSystem, IEnsureComponents {
 
     public TriggerOnEvent[] triggers {
         get {
             return new [] {
                 GameMatcher.Resource.OnEntityRemoved(),
-                Matcher.AllOf(GameMatcher.Resource, GameMatcher.Destroy).OnEntityAdded()
+                GameMatcher.Destroy.OnEntityAdded()
             };
         }
     }
 
     public IMatcher ensureComponents { get { return GameMatcher.View; } }
 
-    public void SetPool(Pool pool) {
-        pool.GetGroup(GameMatcher.View).OnEntityRemoved += onEntityRemoved;
-    }
-
-    void onEntityRemoved(Group group, Entity entity, int index, IComponent component) {
-        var viewComponent = (ViewComponent)component;
-        Object.Destroy(viewComponent.gameObject);
-    }
-
     public void Execute(List<Entity> entities) {
         foreach(var e in entities) {
+            Object.Destroy(e.view.gameObject);
             e.RemoveView();
         }
     }
